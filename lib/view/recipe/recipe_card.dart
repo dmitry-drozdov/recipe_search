48953ec1c36b2aaf351df3a107a41f42f2dc584a
D Drozdov <ds-drozdov@yandex.ru>
2022-07-24 19:03:18 +0300

    favourite icon template

--- lib/view/recipe/recipe_card.dart
+++ lib/view/recipe/recipe_card.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:favorite_button/favorite_button.dart';
 import 'package:flutter/material.dart';
 import 'package:recipe_search/helpers/consts.dart';
 import 'package:recipe_search/helpers/extensions/edge_extension.dart';
@@ -19,38 +20,58 @@ class RecipeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: InkWell(
-        onTap: onTap,
-        child: Container(
-          padding: const EdgeInsets.all(5),
-          constraints: const BoxConstraints(maxHeight: 125),
-          child: Row(
-            children: [
-              CachedNetworkImage(
-                imageUrl: recipe.image,
-                fit: BoxFit.contain,
-                placeholder: (_, __) => placeholder,
-                width: 90,
-              ),
-              Container(
-                width: 270,
-                padding: const EdgeInsets.all(5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      recipe.label,
-                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                      overflow: TextOverflow.ellipsis,
-                    ).paddingV1,
-                    titleValue(title: 'Ingredients: ', value: recipe.ingredientsStr).paddingV1,
-                    titleValue(title: 'Calories: ', value: recipe.caloriesStr).paddingV1,
-                    titleValue(title: 'Weight: ', value: recipe.totalWeightStr).paddingV1,
-                  ],
-                ),
-              ),
-            ],
+      child: Stack(
+        children: [
+          // Icon(Icons.),
+          content(),
+          Positioned(
+            right: 1,
+            top: 1,
+            child: FavoriteButton(
+              isFavorite: true,
+              // iconDisabledColor: Colors.white,
+              valueChanged: (_isFavorite) {
+                print('Is Favorite : $_isFavorite');
+              },
+            ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget content() {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(5),
+        constraints: const BoxConstraints(maxHeight: 125),
+        child: Row(
+          children: [
+            CachedNetworkImage(
+              imageUrl: recipe.image,
+              fit: BoxFit.contain,
+              placeholder: (_, __) => placeholder,
+              width: 90,
+            ),
+            Container(
+              width: 270,
+              padding: const EdgeInsets.all(5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    recipe.label,
+                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    overflow: TextOverflow.ellipsis,
+                  ).paddingV1,
+                  titleValue(title: 'Ingredients: ', value: recipe.ingredientsStr).paddingV1,
+                  titleValue(title: 'Calories: ', value: recipe.caloriesStr).paddingV1,
+                  titleValue(title: 'Weight: ', value: recipe.totalWeightStr).paddingV1,
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );

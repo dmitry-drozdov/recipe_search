0887a2bd65532e5885d185ef91190492d61ab6bd
D Drozdov <ds-drozdov@yandex.ru>
2023-01-03 21:09:29 +0300

    remove debug label

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return KeyboardDismisser(
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Recipe Search',
         theme: ThemeData(
           primarySwatch: Colors.blue,

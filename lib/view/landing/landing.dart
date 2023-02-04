bad5c39bc7e35c558f9702bb0a66a07f57bcb2b2
D Drozdov <ds-drozdov@yandex.ru>
2023-02-04 23:10:30 +0300

    add toggle if use last data or not

--- lib/view/landing/landing.dart
+++ lib/view/landing/landing.dart
@@ -8,21 +8,29 @@ import '../../utils/auth.dart';
 import '../home_navigation.dart';
 import 'google_sign_in_button.dart';
 
-class Landing extends StatelessWidget {
+class Landing extends StatefulWidget {
   final String title;
-  final auth = locator<Authentication>();
 
-  Landing({
+  const Landing({
     Key? key,
     required this.title,
   }) : super(key: key);
+
+  @override
+  State<Landing> createState() => _LandingState();
+}
+
+class _LandingState extends State<Landing> {
+  final auth = locator<Authentication>();
+
+  var useLastKnownData = false;
 
   @override
   Widget build(BuildContext context) {
     final darkColor = Theme.of(context).primaryColorDark;
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -47,7 +55,7 @@ class Landing extends StatelessWidget {
             firstLetterColor: AppColors.redLetter,
           ),
           const SizedBox(height: 50),
-          buttons(context),
+          buttons(),
         ],
       ),
     );
@@ -74,46 +82,62 @@ class Landing extends StatelessWidget {
     );
   }
 
-  Widget buttons(BuildContext ctx) {
+  Widget buttons() {
+    final buttonStyle = TextStyle(color: Theme.of(context).primaryColorDark, fontSize: 16);
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         auth.firebaseApp == null
             ? const Text('Error initializing Firebase')
-            : GoogleSignInButton(onSignIn: (user) => onAuth(ctx, user: user)),
+            : GoogleSignInButton(onSignIn: (googleUser) => onAuthGoogle(googleUser)),
+        Divider(
+          color: AppColors.blueBorder,
+          indent: 25,
+          endIndent: 25,
+        ),
         TextButton(
-          child: const Text('Continue without sign in'),
+          child: Text('Continue without sign in*', style: buttonStyle),
           onPressed: () {
-            onWithoutAuth(ctx);
+            onWithoutAuth();
           },
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("*Use data last saved using Google", style: buttonStyle.copyWith(fontSize: 15)),
+            Switch(
+              inactiveTrackColor: AppColors.lightBlueChip,
+              value: useLastKnownData,
+              onChanged: (val) {
+                if (mounted) setState(() => useLastKnownData = val);
+              },
+            ),
+          ],
         ),
       ],
     );
   }
 
-  void onWithoutAuth(BuildContext ctx) {
-    auth.getLastKnownAppUser().then((appUser) {
-      if (appUser != null) {
-        navigateToMain(ctx, appUser);
-        return;
-      }
-      authLocal(ctx);
-    });
+  Future<void> onWithoutAuth() async {
+    AppUser? appUser;
+    if (useLastKnownData) {
+      appUser = await auth.getLastKnownAppUser();
+    }
+
+    appUser ??= await AppUser.create();
+    navigateToMain(appUser);
   }
 
-  void onAuth(BuildContext ctx, {User? user}) {
-    AppUser.create(googleUser: user).then((appUser) {
-      auth.rememberAppUser(appUser);
-      navigateToMain(ctx, appUser);
-    });
+  Future<void> onAuthGoogle(User googleUser) async {
+    final appUser = await AppUser.create(googleUser: googleUser);
+    auth.rememberAppUser(appUser);
+    navigateToMain(appUser);
   }
 
-  void authLocal(BuildContext ctx) {
-    AppUser.create().then((appUser) => navigateToMain(ctx, appUser));
-  }
-
-  void navigateToMain(BuildContext ctx, AppUser appUser) {
-    Navigator.of(ctx).pushReplacement(
+  void navigateToMain(AppUser appUser) {
+    if (!mounted) return;
+    Navigator.of(context).pushReplacement(
       MaterialPageRoute(
         builder: (_) => HomeNavigation(user: appUser),
       ),
